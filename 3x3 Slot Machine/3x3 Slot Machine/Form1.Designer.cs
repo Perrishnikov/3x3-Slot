cf0e2fdb4f4c1e05d4ed0a9de45a269b3b4fcae5
Perrishnikov <perrishnikov@gmail.com>
2016-11-21 18:30:46 -0600

    Add files via upload

---
+++ 3x3 Slot Machine/3x3 Slot Machine/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace _3x3_Slot_Machine
+{
+    partial class Slots3x3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Slots3x3));
+            this.fruitList = new System.Windows.Forms.ImageList(this.components);
+            this.pnl_boxes = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btn_spin = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // fruitList
+            // 
+            this.fruitList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("fruitList.ImageStream")));
+            this.fruitList.TransparentColor = System.Drawing.Color.Transparent;
+            this.fruitList.Images.SetKeyName(0, "Apple.bmp");
+            this.fruitList.Images.SetKeyName(1, "Banana.bmp");
+            this.fruitList.Images.SetKeyName(2, "Cherries.bmp");
+            this.fruitList.Images.SetKeyName(3, "Grapes.bmp");
+            this.fruitList.Images.SetKeyName(4, "Lemon.bmp");
+            this.fruitList.Images.SetKeyName(5, "Lime.bmp");
+            this.fruitList.Images.SetKeyName(6, "Orange.bmp");
+            this.fruitList.Images.SetKeyName(7, "Pear.bmp");
+            this.fruitList.Images.SetKeyName(8, "Strawberry.bmp");
+            this.fruitList.Images.SetKeyName(9, "Watermelon.bmp");
+            // 
+            // pnl_boxes
+            // 
+            this.pnl_boxes.Location = new System.Drawing.Point(0, 0);
+            this.pnl_boxes.Name = "pnl_boxes";
+            this.pnl_boxes.Size = new System.Drawing.Size(444, 444);
+            this.pnl_boxes.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(450, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(170, 100);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Your Money:";
+            // 
+            // btn_spin
+            // 
+            this.btn_spin.Location = new System.Drawing.Point(450, 118);
+            this.btn_spin.Name = "btn_spin";
+            this.btn_spin.Size = new System.Drawing.Size(75, 23);
+            this.btn_spin.TabIndex = 3;
+            this.btn_spin.Text = "Spin";
+            this.btn_spin.UseVisualStyleBackColor = true;
+            this.btn_spin.Click += new System.EventHandler(this.btn_spin_Click);
+            // 
+            // Slots3x3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(647, 417);
+            this.Controls.Add(this.btn_spin);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pnl_boxes);
+            this.Name = "Slots3x3";
+            this.Text = "Slot Machine Fun";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ImageList fruitList;
+        private System.Windows.Forms.Panel pnl_boxes;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btn_spin;
+    }
+}
+
---
+++ 3x3 Slot Machine/3x3 Slot Machine/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3x3_Slot_Machine
+{
+    public partial class Slots3x3 : Form
+    {
+        static int ROW = 3, COL = 3;
+        PictureBox[,] masterBoxes = new PictureBox[ROW, COL]; //array to hold all picboxes
+        List<string> fruitsList = new List<string>(); //temp List to hold all fruits presently in panel. Reset on every spin. 
+        //will only check fruit in here
+
+        public Slots3x3()
+        {
+            InitializeComponent();
+            makeBoxes();
+        }
+
+        private void btn_spin_Click(object sender, EventArgs e)
+        {
+            insertImages(); //void to insert images into DOM
+
+            List<string> flatList = flattenArray(masterBoxes); //returns list of true if FRUIT == TAG
+
+            bool result = isWin(flatList, fruitsList, win_ThreeInRow()); //compare Master, to win conditions
+
+            if (result)
+            {
+                MessageBox.Show("Win");
+            }
+        }
+
+        private bool isWin(List<string> flattenedList, List<string> fruits, int[][] WinValues)
+        {
+            //flattenedList is string of "cherries.bmp"
+            //fruits is is string of "cherries.bmp"
+            //WinValues is 8 length, 0-7ints; WinValues[row] is 3 length, 0-8 ints
+
+            var flag = false; //change to true when combo is found
+            foreach(var fruit in fruits) //grab a fruit...
+            {
+                for (int row = 0; row < WinValues.Length; row++) //grab a jagged array...
+                {
+                    List<int> locations = new List<int>(); //List to hold winning combos
+
+                    for (int col = 0; col < WinValues[row].Length; col++) //grab the combo in the array...
+                    {
+                        
+                        if (flattenedList[WinValues[row][col]] == fruit) //check each location; matching combo vs flattened array...
+                        {
+                            locations.Add(WinValues[row][col]); //add matching location to list
+
+                            if (locations.Count == WinValues[row].Length) //Win if all 3 matched
+                            {
+                                Console.WriteLine("Win!"); //log winning combo
+                                foreach (var i in locations) {
+                                    Console.WriteLine(i.ToString());
+                                }
+                                flag = true;
+                            }
+                        }
+                    }
+                }
+            }
+            return flag;
+        }
+
+        private List<string> flattenArray(PictureBox[,] incoming) //return fruit string "cherries"
+        {
+            List<string> tempList = new List<string>(); //9
+            foreach (var item in incoming) //make incoming into string "cherries"
+            {
+                tempList.Add(item.Tag.ToString());
+            }
+            return tempList;
+        }
+
+        private int[][] win_ThreeInRow()
+        {
+            int[][] combos = new int[8][];
+
+            //Create each array that is an element of jagged array
+            combos[0] = new int[3] { 0, 4, 8 };
+            combos[1] = new int[3] { 1, 4, 7 };
+            combos[2] = new int[3] { 2, 4, 6 };
+            combos[3] = new int[3] { 3, 4, 5 };
+            combos[4] = new int[3] { 0, 1, 2 }; //top
+            combos[5] = new int[3] { 2, 5, 8 }; //right
+            combos[6] = new int[3] { 6, 7, 8 }; //bottom
+            combos[7] = new int[3] { 0, 3, 6 }; //left
+
+            return combos;
+        }
+
+        private void insertImages()
+        {
+            Random rand = new Random();
+            //Array.Clear(masterBoxes, 0, masterBoxes.Length);
+
+            fruitsList.Clear();
+
+            foreach (PictureBox picture in masterBoxes)
+            {
+                int nextRand = rand.Next(10);
+
+                picture.Image = fruitList.Images[nextRand]; //assign random image to each picbox
+                picture.Tag = fruitList.Images.Keys[nextRand].ToString(); //assign image name to each picbox
+                fruitsList.Add(picture.Tag.ToString()); //add each tag(fruit) to the fruits List
+                pnl_boxes.Controls.Add(picture); //render picture in the panel
+                //Console.WriteLine(picture.Tag.ToString());
+            }
+        }
+
+        private void makeBoxes()
+        {
+            int padding = 15; //padding in form
+
+            for (int row = 0; row < ROW; row++)
+            {
+                for (int col = 0; col < COL; col++)
+                {
+                    //Console.WriteLine("row: " + row + " col: " + col);
+                    PictureBox picture = new PictureBox
+                    {
+                        Name = "peanutButter" + row + col,
+                        Size = new Size(128, 128),
+                        Location = new Point((col * 128) + padding, (row * 128) + padding), //postion each picbox so dont overlap
+                        BackColor = Color.FromArgb(255, 249, 189),
+                        BorderStyle = BorderStyle.FixedSingle
+                    };
+                    //Console.WriteLine(picture.Tag);
+                    masterBoxes[row, col] = picture; //push picture into masterBoxes array
+                    pnl_boxes.Controls.Add(picture); //render backcolor in the panel
+                };
+            }
+        }
+    }
+}
